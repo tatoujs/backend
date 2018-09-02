ae241b316d1855d259cdf3231157e069d317d9cd
Florian Adonis <tanohzana@Florians-MacBook-Pro.local>
2018-09-02 12:03:54 +0200

    wip: initiated tests

--- spec/bookService.test.js
+++ spec/bookService.test.js
@@ -4,7 +4,8 @@ const bookService = require('../services/bookService');
 
 describe('A book service', () => {
 
-    it('should pass this test', () => {
-       expect(true).toBe(true);
+    it('should retrieve 20 books', () => {
+        // bookService.getBooks()
+        expect(true).toBe(true);
     });
 });
