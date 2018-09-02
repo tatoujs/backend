6a0d6f9a683b97ff0cd9f3adfd26e1d25478294c
Florian Adonis <tanohzana@Florians-MacBook-Pro.local>
2018-09-02 11:53:55 +0200

    fix: added a missing file

---
+++ spec/bookService.test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const bookService = require('../services/bookService');
+
+describe('A book service', () => {
+
+    it('should pass this test', () => {
+       expect(true).toBe(true);
+    });
+});
