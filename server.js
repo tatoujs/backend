74b1ff71b0a8eebbfe96a03e7b6e978800e818e1
Florian Adonis <tanohzana@Florians-MacBook-Pro.local>
2018-11-10 13:05:42 +0100

    fix: refactored code es6 way

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ import nodemailer from 'nodemailer'
 import jwt from 'jsonwebtoken'
 import http from 'http'
 import SocketIO from 'socket.io'
+import Stripe from 'stripe'
 
-import config from 'config' // we load the db location from the JSON files
+// we load the db location from the JSON files
+import config from 'config'
 
 import bookService from './services/bookService'
 import chatService from './services/chatService'
@@ -32,111 +34,105 @@ const port = 8080
 
 // db options
 const options = {
-    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
-    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } }
-};
+  server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } },
+}
 
 // db connection
-mongoose.connect(config.get("DBHost"), options);
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connect(config.get('DBHost'), options)
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
 
 // key to generate token for jwt secure connexions
-const SYNCH_KEY = config.get("synch_key");
+const SYNC_KEY = config.get('sync_key')
 
 // configures push notifications service
-onesignal.configure(config.get("one_signal_app_id"), config.get("one_signal_api_key"));
+onesignal.configure(config.get('one_signal_app_id'), config.get('one_signal_api_key'))
 
 // configures Stripe payment service
-const stripe = require("stripe")(config.get("stripeSecretKey"));
+const stripe = new Stripe(config.get('stripeSecretKey'))
 
 // configures nodemailer to send and receive mails on hello@kimli.fr
 const transporter = nodemailer.createTransport({
-    host:'smtp.office365.com',
-    port:587,
-    secure:false,
-    tls: {
-        ciphers:'SSLv3'
-    },
-    auth:{
-        user:'hello@kimli.fr',
-        pass:'/kimliKimli2/'
-    }
-});
+  host: 'smtp.office365.com',
+  port: 587,
+  secure: false,
+  tls: {
+    ciphers: 'SSLv3',
+  },
+  auth: {
+    user: 'hello@kimli.fr',
+    pass: '/kimliKimli2/',
+  },
+})
 
-// SHOULD PROBABLY BE REMOVED
+const store = new ExpressBrute.MemoryStore()
+const bruteforce = new ExpressBrute(store)
 
-// var credentials = {
-//     IonicApplicationID : "06196fee",
-//     IonicApplicationAPItoken : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwMzVmMzVjNS00ZmM3LTRjNGQtYjU0MC02ODc0ZDc0NGU2MmYifQ.8PWmRz3dE3s7DVr_QbCoig_ZPp_g_2yDdqQE3HF5IGk"
-// };
-
-const store = new ExpressBrute.MemoryStore();
-const bruteforce = new ExpressBrute(store);
-
-//don't show the log when it is test
-if(config.util.getEnv('NODE_ENV') !== 'test') {
-    //use morgan to log at command line
-    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+// Do not show logs when it is test
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+    // use morgan to log at command line
+    // 'combined' outputs the Apache style LOGs
+    app.use(morgan('combined'))
 }
 
-
 // 11 modules that increase NodeJS app security
-app.use(helmet());
+app.use(helmet())
 
-//parse application/json and look for raw text
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/json'}));
+// parse application/json and look for raw text
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text())
+app.use(bodyParser.json({ type: 'application/json' }))
 
-app.set('port', (process.env.PORT || 5004));
+app.set('port', (process.env.PORT || 5004))
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
+  // website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+  // request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  // request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Access-Control-Allow-Origin, Content-Type, Authorization, Content-Length, auth_token')
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Access-Control-Allow-Origin, Content-Type, Authorization, Content-Length, auth_token');
+  // set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+  // pass to next layer of middleware
+  next()
+})
 
-    // Pass to next layer of middleware
-    next();
-});
+app.disable('x-powered-by')
 
-app.disable('x-powered-by');
+app.get('/', (req, res) => res.json({ message: 'Welcome to Kimli !' }))
 
-app.get("/", (req, res) => res.json({message: "Welcome to Kimli ! "}));
-
-//////////// BOOKS ///////////////
+///////////// BOOKS ///////////////
 
 app.route("/books")
-    .get(bookService.getBooks)
-    .post(bookService.postBook);
+  .get(bookService.getBooks)
+  .post(bookService.postBook)
 
 app.route("/books/:id")
-    .get(bookService.getBook)
-    .put(bookService.updateBook);
+  .get(bookService.getBook)
+  .put(bookService.updateBook)
 
 ///////////// CHATS //////////////
 
 app.route("/chats")
-    .post(chatService.addChat);
+  .post(chatService.addChat)
 
 app.route("/chats/:userId")
-    .get(chatService.getChatsByUserId)
-    .put(chatService.updateChat);
+  .get(chatService.getChatsByUserId)
+  .put(chatService.updateChat)
 
 //////////////////////////////////
-app.listen(port);
 
-console.log("Magic is happening on port " + port);
+app.listen(port)
 
-module.exports = app; // for testing
+console.log(`Magic is happening on port ${port}`)
+
+// for testing
+module.exports = app
--- services/chatService.js
+++ services/chatService.js
@@ -1,77 +1,83 @@
-"use strict";
+import Chat from '../models/chatModel'
+import ChatValidator from '../utils/chatValidator'
 
-const Chat = require("../models/chatModel");
-const ChatValidator = require("../utils/chatValidator");
+const getChatsByUserId = (req, res) => {
+  const { userId } = req.params
 
-let getChatsByUserId = (req, res) => {
-    if(req.params.userId) {
-        let userId = req.params.userId;
-        let query = Chat.find().or([{ 'user1._id': userId }, { 'user2._id': userId }]);
-        query.exec((err, chats) => {
-           if(err){
-               res.status(500);
-               res.send(err);
-           }
-           console.log(chats);
-           res.json(chats);
-        });
-    } else {
-        res.status(400);
-        res.send("No userId specified");
-    }
-};
+  if (userId) {
+    const query = Chat.find().or([{ 'user1._id': userId }, { 'user2._id': userId }])
 
-let addChat = (req, res) => {
-  if(ChatValidator.isValid(req.body)) {
-      let chat = new Chat(req.body);
-      chat.save((err, newChat) => {
-          if(err) {
-              res.status(500);
-              res.send(err);
-          }
-          console.log(newChat);
-          res.json(newChat);
-      });
-  } else {
-      res.status(400);
-      res.send("Invalid Chat object");
-  }
-};
-
-let updateChat = (req, res) => {
-    Chat.findById({'_id': req.params._id}, (err, chat) => {
-        if(err) {
-            res.status(500);
-            res.send(err);
-        } else if(!chat) {
-            res.status(404);
-            res.send("Wrond chat id");
-        } else {
-            Object.assign(chat, req.body).save((err, chatUpdated) => {
-                if(err) {
-                    res.status(500);
-                    res.send(err);
-                } else {
-                    res.status(204);
-                    // TODO: VERIFY WELL 204 AND NOT 200 OTHERWISE ADD {}
-                    res.send();
-                }
-            });
-        }
-    });
-};
-
-let deleteChat = (req, res) => {
-  Chat.remove({_id: req.params._id}, (err, result) => {
-      if(err) {
-          res.status(500);
-          res.send(err);
+    query.exec((err, chats) => {
+      if (err) {
+        res.status(500)
+        res.send(err)
       } else {
-          res.status(204);
-          // TODO: VERIFY WELL 204 AND NOT 200 OTHERWISE ADD {}
-          res.send();
+        res.json(chats)
       }
-  })
-};
+    })
+  } else {
+    res.status(400)
+    res.send("no userId specified")
+  }
+}
 
-module.exports = { getChatsByUserId, addChat, updateChat, deleteChat };
+const addChat = (req, res) => {
+  if (ChatValidator.isValid(req.body)) {
+    const chat = new Chat(req.body)
+
+    chat.save((err, newChat) => {
+      if (err) {
+        res.status(500)
+        res.send(err)
+      }
+
+      res.json(newChat)
+    })
+  } else {
+    res.status(400)
+    res.send("invalid Chat object")
+  }
+}
+
+const updateChat = (req, res) => {
+  const { _id } = req.params
+  const chatFieldsToUpdate = req.body
+
+  Chat.findById({ _id }, (err, chat) => {
+    if (err) {
+      res.status(500)
+      res.send(err)
+    } else if (!chat) {
+      res.status(404)
+      res.send("wrong chat id")
+    } else {
+      Object.assign(chat, chatFieldsToUpdate).save((err, chatUpdated) => {
+        if (err) {
+          res.status(500)
+          res.send(err)
+        } else {
+          res.status(204)
+          // TODO: VERIFY WELL 204 AND NOT 200 OTHERWISE ADD {}
+          res.send()
+        }
+      })
+    }
+  })
+}
+
+const deleteChat = (req, res) => {
+  const { _id } = req.params
+
+  Chat.remove({ _id }, (err, result) => {
+    if (err) {
+      res.status(500)
+      res.send(err)
+    } else {
+      res.status(204)
+      // TODO: VERIFY WELL 204 AND NOT 200 OTHERWISE ADD {}
+      res.send()
+    }
+  })
+}
+
+module.exports = { getChatsByUserId, addChat, updateChat, deleteChat }
